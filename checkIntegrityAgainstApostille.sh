828c48223fdf27ab59475c9a3a1fa086d077ced0
whatilike <whatilike@fultus.net>
2018-03-08 12:48:29 +0200

    Heavy lifting of script. Checks for required programs. Supports following hashes MD5, SHA1, SHA256, SHA3. Will display message if hash contains one.

--- checkIntegrityAgainstApostille.sh
+++ checkIntegrityAgainstApostille.sh
@@ -3,10 +3,14 @@
 # This tool will validate integrity of file against cryptographic hash that is fetched from NEM blockchain using
 # "txId" parameter found from ".sig" file.
 #
-# Cryptographic (this script supports: MD5, SHA1 and SHA256) hash will be downloaded from NEM blockchain 
-# and validated against ryptographic hash gotten from file.
+# Cryptographic (this script supports: MD5, SHA1, SHA256 and SHA3) hash will be downloaded from NEM blockchain 
+# and validated against cryptographic hash gotten from file.
 
-# This script depends on programs: curl, jq, sed, sha1sum, sha256sum
+# This script depends on programs: curl, jq, sed, sha1sum, sha256sum and sha3sum
+# Ubuntu: sha3sum is provided from "libdigest-sha3-perl" package
+# Fedora / RHEL: sha3sum is provided by "sha3sum" package
+
+MISSINGPROGS=""
 
 if [ "$1" == "" ]; then
     echo "Please provide file to check integrity"
@@ -33,32 +37,46 @@ fi
 
 #Check for programs
 if [ ! -f /usr/bin/jq ]; then
-    echo "Program \"jq\" missing. Aborting..."
-    exit
+    MISSINGPROGS="${MISSINGPROGS} jq"
 fi
 
 if [ ! -f /bin/sed ]; then
-    echo "Program \"sed\" missing. Aborting..."
-    exit
+    MISSINGPROGS="${MISSINGPROGS} sed"
 fi
 
 if [ ! -f /usr/bin/curl ]; then
-    echo "Program \"curl\" missing. Aborting..."
-    exit
+    MISSINGPROGS="${MISSINGPROGS} curl"
 fi
 
 if [ ! -f /usr/bin/md5sum ]; then
-    echo "Program \"sha256sum\" missing. Aborting..."
-    exit
+    MISSINGPROGS="${MISSINGPROGS} md5sum"
 fi
 
 if [ ! -f /usr/bin/sha1sum ]; then
-    echo "Program \"sha256sum\" missing. Aborting..."
-    exit
+    MISSINGPROGS="${MISSINGPROGS} sha1sum"
 fi
 
 if [ ! -f /usr/bin/sha256sum ]; then
-    echo "Program \"sha256sum\" missing. Aborting..."
+    MISSINGPROGS="${MISSINGPROGS} sha256sum"
+fi
+
+if [ ! -f /usr/bin/sha3sum ]; then
+    MISSINGPROGS="${MISSINGPROGS} sha3sum"
+fi
+
+function hex2string () {
+  I=0
+  while [ $I -lt ${#1} ];
+  do
+    echo -en "\x"${1:$I:2}
+    let "I += 2"
+  done
+}
+
+if [ ${#MISSINGPROGS} -gt 2 ]; then
+    echo "Following programs missing: $MISSINGPROGS"
+    echo "NOTE: sha3sum is found from ubuntu package libdigest-sha3-perl, Fedora/RHEL package is sha3sum"
+    echo "Aborting..."
     exit
 fi
 
@@ -75,6 +93,8 @@ fi
 checksum=${apostilleHash:0:10}
 hashingVersionBytes=${checksum:8:2}
 cryptoHash=${apostilleHash:10}
+fileHash=""
+message=""
 signed=${hashingVersionBytes:1}
 
 if [ "$signed" == "8" ]; then
@@ -84,20 +104,33 @@ fi
 
 if [ "$hashingVersionBytes" == "01" ]; then
     hashFunction="md5sum"
+    fileHash=${cryptoHash:0:32}
+    message=${cryptoHash:36}
 elif [ "$hashingVersionBytes" == "02" ]; then
     hashFunction="sha1sum"
+    fileHash=${cryptoHash:0:40}
+    message=${cryptoHash:44}
 elif [ "$hashingVersionBytes" == "03" ]; then    
     hashFunction="sha256sum"
+    fileHash=${cryptoHash:0:64}
+    message=${cryptoHash:68}
 elif [ "$hashingVersionBytes" == "08" ]; then
-    echo "256bit SHA3 not supported yet. Aborting..."
-    exit
+    hashFunction="sha3sum -a 256"
+    fileHash=${cryptoHash:0:64}
+    message=${cryptoHash:68}
 else 
-    echo "512bit SHA3 not supported yet. Aborting..."
-    exit
+    hashFunction="sha3sum -a 512"
+    fileHash=${cryptoHash:0:128}
+    message=${cryptoHash:132}
 fi
 
-echo "$cryptoHash $1" > /tmp/validateNemSignature.tmp.$1
+echo "$fileHash $1" > /tmp/validateNemSignature.tmp.$1
 
 eval "$hashFunction -c /tmp/validateNemSignature.tmp.$1"
 
+if [ ${#message} -gt 0 ]; then
+    echo "Apostille hash contained also following message:"
+    hex2string $message
+fi
+echo ""
 rm /tmp/validateNemSignature.tmp.$1
